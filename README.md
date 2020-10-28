0c15d2101874bf447d8448a95911df97240e6bc2
Simon Kadisch <simon.kadisch@k13-engineering.at>
2020-10-29 00:57:04 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # node-loopdev
 Loopdevice library for Node.js
+
+This library will help you to create loop devices in Linux without invoking shell commands like losetup. Managing existing loop devices is not part of this library, as the main use-case is creating loop devices for mounting filesystems.
+
+## API
+
+### `loopdev.create(options)` => Promise(`LoopDevice`)
+
+Creates a loop device instance.
+
+- `options.fd`: fd of backing file
+- `options.offset`: offset in backing file (optional, default = 0)
+- `options.sizelimit`: size limit of backing file (optional, default = unlimited)
+- `options.readOnly`: whether loop device should be read-only (optional, default = false)
+- `options.autoclear`: remove loop device after last file descriptor closed (optional, default = true)
+  - Requires Linux >= 2.6.25
+  
+- `options.partscan`: scan for partitions after loop device is created (optional, default = false)
+
+- `options.useDirectIo`: use direct I/O, requires Linux 4.10 (optional, default = false)
+  - Requires Linux >= 4.10
+
+- `options.blockSize`: use block size of loop devices (optional)
+  - Requires Linux >= 4.14
+
+### `loopdev.createFromPath(options)` => Promise(`LoopDevice`)
+
+Convenience method for `loopdev.create()`;
+
+- `options.path`: path of backing file
+
+For all other options see `loopdev.create()`. A file descriptor `options.fd` may not be given, as it is created internally from `options.path`.
+
+### `LoopDevice`
+
+#### `LoopDevice.devicePath` => String
+
+Exposes loop device path (e.g. "/dev/loop0").
+
+#### `LoopDevice.destroy()` => Promise
+
+Destroys the loop device. This causes the loop device to detach the backing file. Also closes the handle to the loop device. After this call the `LoopDevice` is unusable.
+
+#### `LoopDevice.close()` => Promise
+
+Closes the handle to the loop device. If the `autoclear` option has been used, and no other peer has open file descriptors to loop device (or mounts), then it will destroy itself. Closing the handle is required in any case, except when `LoopDevice.destroy()` is called, even if you want the loop device to stay available in the system longer than your node app. After this call the `LoopDevice` is unusable.
---
+++ test/basic.js
@@ -0,0 +1,304 @@
+/* global describe */
+/* global it */
+
+import loopdev from "../lib/index.js";
+import tmp from "tmp-promise";
+import fs from "fs";
+import assert from "assert";
+import { performance } from "perf_hooks";
+
+const testImageContent = Buffer.alloc(1 * 1024 * 1024);
+const testString = "Hello, World!";
+Buffer.from(testString).copy(testImageContent);
+
+const maybeTest = (test) => {
+  if (test) {
+    return it;
+  } else {
+    return it.skip.bind(it);
+  }
+};
+
+const withTestImage = (fn) => {
+  return tmp.withFile(async ({ path, fd }) => {
+    await new Promise((resolve, reject) => {
+      fs.write(fd, testImageContent, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+    await fn({ path, fd });
+  });
+};
+
+const readSizeOfBlockDevice = async ({ path }) => {
+  const m = path.match(/\/dev\/(\S+)/);
+  if (!m) {
+    throw new Error(`unsupported block device path ${path}`);
+  }
+
+  const blockDeviceName = m[1];
+
+  const sizeAsStr = await fs.promises.readFile(`/sys/block/${blockDeviceName}/size`, "utf8");
+  return parseInt(sizeAsStr, 10) * 512;
+};
+
+const waitUntil = async (fn, { timeout = 30000 } = {}) => {
+  const start = performance.now();
+  const end = start + timeout;
+
+  let now;
+  do {
+    const result = await fn();
+    if (result === true) {
+      return;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 50));
+
+    now = performance.now();
+  } while (now < end);
+
+  throw new Error("timeout");
+};
+
+const basicLoopDeviceCheck = ({ dev }) => {
+  assert(typeof dev.devicePath === "string");
+  assert(dev.devicePath.startsWith("/dev/loop"));
+};
+
+const loopDeviceContentCheck = async ({ dev, offset = 0 }) => {
+  const sizeOfDev = await readSizeOfBlockDevice({ "path": dev.devicePath });
+  const contentOfDev = await fs.promises.readFile(dev.devicePath, "utf8");
+
+  const expectedSize = parseInt((testImageContent.length - offset) / 512, 10) * 512;
+
+  assert.equal(sizeOfDev, expectedSize);
+  assert.equal(contentOfDev.substr(0, testString.length - offset), testString.substr(offset));
+};
+
+const waitForLoopDeviceDissapear = async ({ dev }) => {
+  // unfortunately there seems to be a bit of delay when using autoclear
+  // altough we closed all file descriptors it still is open
+  // maybe node's file descriptors are not really closed or the kernel
+  // does cleanup asynchronously
+
+  await waitUntil(async () => {
+    const sizeOfDevAfter = await readSizeOfBlockDevice({ "path": dev.devicePath });
+    return sizeOfDevAfter === 0;
+  });
+};
+
+describe("loopdev", function () {
+  this.timeout(60 * 1000);
+
+  it("should create loop device correctly", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+
+        const sizeOfDev = await readSizeOfBlockDevice({ "path": dev.devicePath });
+        assert.equal(sizeOfDev, testImageContent.length);
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+
+  it("should create loop device from path correctly", async () => {
+    await withTestImage(async ({ path }) => {
+      const dev = await loopdev.createFromPath({ path });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+
+        const sizeOfDev = await readSizeOfBlockDevice({ "path": dev.devicePath });
+        assert.equal(sizeOfDev, testImageContent.length);
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+
+  it("should give an error when path and fd are given to createFromPath", async () => {
+    await withTestImage(async ({ fd, path }) => {
+      await assert.rejects(async () => {
+        await loopdev.createFromPath({ fd, path });
+      });
+    });
+  });
+
+  it("should give an error when unknown arguments are given", async () => {
+    await withTestImage(async ({ fd }) => {
+      await assert.rejects(async () => {
+        await loopdev.createFromPath({ fd, "myArg": false });
+      });
+    });
+  });
+
+  it("should destroy loop device correctly", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd });
+      basicLoopDeviceCheck({ dev });
+
+      const sizeOfDevBefore = await readSizeOfBlockDevice({ "path": dev.devicePath });
+      assert.equal(sizeOfDevBefore, testImageContent.length);
+      await dev.destroy();
+
+      await waitForLoopDeviceDissapear({ dev });
+    });
+  });
+
+  it("should destroy loop device with autoclear correctly", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd, "autoclear": true });
+      basicLoopDeviceCheck({ dev });
+      const sizeOfDevBefore = await readSizeOfBlockDevice({ "path": dev.devicePath });
+      assert.equal(sizeOfDevBefore, testImageContent.length);
+      await dev.close();
+
+      await waitForLoopDeviceDissapear({ dev });
+    });
+  });
+
+  it("should fail on invalid block size", async () => {
+    await withTestImage(async ({ fd }) => {
+      let dev;
+
+      await assert.rejects(async () => {
+        dev = await loopdev.create({ fd, "blockSize": 513 });
+        await dev.destroy();
+      });
+    });
+  });
+
+  [
+    512,
+    1024,
+    2048,
+    4096
+  ].forEach((blockSize) => {
+    const skipTest = process.env.NODE_LOOPDEV_TEST_SKIP_BLOCK_SIZE;
+    maybeTest(!skipTest)(`should create loop device with blockSize=${blockSize} correctly`, async () => {
+      await withTestImage(async ({ fd }) => {
+        const dev = await loopdev.create({ fd, blockSize });
+        try {
+          basicLoopDeviceCheck({ dev });
+          await loopDeviceContentCheck({ dev });
+        } finally {
+          await dev.destroy();
+        }
+      });
+    });
+  });
+
+  [
+    1,
+    2,
+    3,
+    4
+  ].forEach((offset) => {
+    it(`should create loop device with offset=${offset} correctly`, async () => {
+      await withTestImage(async ({ fd }) => {
+        const dev = await loopdev.create({ fd, offset });
+        try {
+          basicLoopDeviceCheck({ dev });
+          await loopDeviceContentCheck({ dev, offset });
+        } finally {
+          await dev.destroy();
+        }
+      });
+    });
+  });
+
+  [
+    512,
+    1024,
+    2048,
+    4096
+  ].forEach((sizelimit) => {
+    it(`should create loop device with sizelimit=${sizelimit} correctly`, async () => {
+      await withTestImage(async ({ fd }) => {
+        const dev = await loopdev.create({ fd, sizelimit });
+        try {
+          basicLoopDeviceCheck({ dev });
+
+          const sizeOfDev = await readSizeOfBlockDevice({ "path": dev.devicePath });
+          const contentOfDev = await fs.promises.readFile(dev.devicePath, "utf8");
+
+          assert.equal(sizeOfDev, sizelimit);
+          assert.equal(contentOfDev.substr(0, testString.length), testString);
+        } finally {
+          await dev.destroy();
+        }
+      });
+    });
+  });
+
+  it("should create loop device with partscan=true correctly", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd, "partscan": true });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+
+  maybeTest(!process.env.NODE_LOOPDEV_TEST_SKIP_DIRECT_IO)("should create loop device with useDirectIo=true correctly", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd, "useDirectIo": true });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+
+  it("should be writable when readOnly=false", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd, "readOnly": false });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+
+        const writeContent = "test!";
+
+        await fs.promises.writeFile(dev.devicePath, writeContent);
+
+        const contentOfDev = await fs.promises.readFile(dev.devicePath, "utf8");
+        assert.equal(contentOfDev.substr(0, writeContent.length), writeContent);
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+
+  // not working, I would expect an error when trying to write to loop device
+  it.skip("should not be writable when readOnly=true", async () => {
+    await withTestImage(async ({ fd }) => {
+      const dev = await loopdev.create({ fd, "readOnly": true });
+      try {
+        basicLoopDeviceCheck({ dev });
+        await loopDeviceContentCheck({ dev });
+
+        const writeContent = "test!";
+
+        await assert.rejects(async () => {
+          await fs.promises.writeFile(dev.devicePath, writeContent);
+        });
+      } finally {
+        await dev.destroy();
+      }
+    });
+  });
+});
